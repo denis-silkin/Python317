5cb6edb17f918cafd665dbb8f6391fc17ec497dd
denis-silkin <denis-silkin@yandex.ru>
2024-02-07 18:03:13 +0300

    dz_py_10

--- dz_python/dz_py_10.py
+++ dz_python/dz_py_10.py
@@ -1,8 +1,10 @@
 # Homework_10
 
 
-matematika = {'Матвей', 'Евгения', 'Михаил', 'Максим', 'Наталья'}
-fizika = {'Максим', 'Матвей', 'Александр'}
-full = matematika | fizika
-print(full)
-print(matematika & fizika)
+matematika = ['Матвей', 'Евгения', 'Михаил', 'Максим', 'Наталья']
+fizika = ['Максим', 'Матвей', 'Александр']
+print("Все призеры:", list(set(matematika + fizika)))
+print('Призеры обеих олимпиад:', set(matematika) & set(fizika))
+print('Обновленный список призеров по математике:', set(matematika) & set(fizika))
+del fizika
+
