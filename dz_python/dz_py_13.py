1919ba90a103a2b5506739c3158ce5b22e9de9ba
denis-silkin <denis-silkin@yandex.ru>
2024-04-01 17:11:19 +0300

    dz_py_13

--- dz_python/dz_py_13.py
+++ dz_python/dz_py_13.py
@@ -1,40 +1,53 @@
 # Homework_13
 
-# Вариант 1
+# def para(a, b, c):
+#     def rect(x, y):
+#         return x * y
+#
+#     s = 2 * (rect(a, b) + rect(a, c) + rect(b, c))
+#     return s
+#
+#
+# print(para(2, 4, 6))
+# print(para(5, 8, 2))
+# print(para(1, 6, 8))
 
+# -----------------------------------------
+
+# s = 0
+#
+#
+# def para(a, b, c):
+#     def rect(x, y):
+#         return x * y
+#
+#     global s
+#     s = 2 * (rect(a, b) + rect(a, c) + rect(b, c))
+#     return s
+#
+#
+# para(2, 4, 6)
+# print(s)
+# para(5, 8, 2)
+# print(s)
+# para(1, 6, 8)
+# print(s)
+
+# ---------------------------------------------------
 
 def para(a, b, c):
-    s = 2 * (a * b + a * c + b * c)
-    print('Площадь параллелепипеда:', s)
+    s = 0
 
-    def rect():
-        nonlocal a, b
-        sr = a * b
-        print('Площадь прямоугольника:', sr)
-    rect()
+    def rect(x, y):
+        nonlocal s
+        s += x * y
+
+    rect(a, b)
+    rect(a, c)
+    rect(b, c)
+    return 2 * s
 
 
 print(para(2, 4, 6))
-
-
-# Вариант 2
-
-
-# def para():
-#     a = int(input('Введите значение: '))
-#     b = int(input('Введите значение: '))
-#     c = int(input('Введите значение: '))
-#     s = 2 * (a * b + a * c + b * c)
-#     print('Площадь параллелепипеда:', s)
-#
-#     def rect():
-#         a = int(input("Введите число a: "))
-#         b = int(input("Введите число b: "))
-#         sr = a * b
-#         print('Площадь прямоугольника:', sr)
-#     return rect
-#
-#
-# res = para()
-# print(res())
-
+print(para(5, 8, 2))
+print(para(1, 6, 8))
