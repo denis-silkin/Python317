278a9337cf6ebf4c7f5a464fbf8f196a8aadf519
denis-silkin <denis-silkin@yandex.ru>
2024-06-07 20:05:49 +0300

    dz_py_43

---
+++ dz_python/dz_py_43/templates/dz_py_43.py
@@ -0,0 +1,20 @@
+# Homework_43
+
+from jinja2 import Template
+
+html = """
+{%- macro get_input(type='text', name='', placeholder='') -%}
+        <input type= '{{ type }}' name='{{ name }}' placeholder='{{ placeholder }}'>
+{%- endmacro -%}
+
+<p>{{ get_input(name='firstname', placeholder='Имя') }}</p>
+<p>{{ get_input(name='lastname', placeholder='Фамилия') }}</p>
+<p>{{ get_input(name='address', placeholder='Адрес') }}</p>
+<p>{{ get_input(type='tel', name='phone', placeholder='Телефон') }}</p>
+<p>{{ get_input(type='email', name='email', placeholder='Почта') }}</p>
+"""
+tm = Template(html)
+msg = tm.render()
+
+print(msg)
+
